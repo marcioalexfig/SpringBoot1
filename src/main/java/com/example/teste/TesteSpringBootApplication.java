38351cd061ae0802e4c8c0eba1a60d2f306523d3
Alex <alex@Mac-mini-de-Alex.local>
2017-09-11 20:21:54 -0300

    subindo codigo de exemplo

---
+++ src/main/java/com/example/teste/TesteSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringBootApplication.class, args);
+		System.out.println("teste");
+		
+	}
+}
